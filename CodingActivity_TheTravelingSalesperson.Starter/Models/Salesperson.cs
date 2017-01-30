2af93ba6962ee31caf68e9867f2381a1711bfe9b
Ryan Parlin <parlinr@ad.nmc.edu>
2017-01-30 17:07:24 -0500

    Initial commit

---
+++ CodingActivity_TheTravelingSalesperson.Starter/Models/Salesperson.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TheTravelingSalesperson
+{
+    public class Salesperson
+    {
+        #region FIELDS
+
+        private string _firstName;
+
+        #endregion
+        
+        #region PROPERTIES
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        #endregion
+
+
+        #region CONSTRUCTORS
+
+        public Salesperson()
+        {
+
+        }
+
+        public Salesperson(string firstName)
+        {
+            _firstName = firstName;
+        }
+
+        #endregion
+
+
+        #region METHODS
+
+
+
+        #endregion
+    }
+}
---
+++ CodingActivity_TheTravelingSalesperson.Starter/Views/ConsoleView.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Text;
+
+namespace CodingActivity_TheTravelingSalesperson
+{
+    /// <summary>
+    /// Console class for the MVC pattern
+    /// </summary>
+    public class ConsoleView
+    {
+        #region FIELDS
+
+        //
+        // declare a Salesperson object for the Controller to use
+        // Note: There is no need for a Salesperson property given the Controller already 
+        //       has access to the same Salesperson object.
+        //
+        private Salesperson _salesperson;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// default constructor to create the console view objects
+        /// </summary>
+        public ConsoleView(Salesperson salesperson)
+        {
+            _salesperson = salesperson;
+
+            InitializeConsole();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// initialize all console settings
+        /// </summary>
+        private void InitializeConsole()
+        {
+            ConsoleUtil.WindowTitle = "- add window title text -";
+            ConsoleUtil.HeaderText = " - add initial header text -";
+        }
+
+        /// <summary>
+        /// display the Continue prompt
+        /// </summary>
+        public void DisplayContinuePrompt()
+        {
+            Console.CursorVisible = false;
+
+            Console.WriteLine();
+
+            ConsoleUtil.DisplayMessage("Press any key to continue.");
+            ConsoleKeyInfo response = Console.ReadKey();
+
+            Console.WriteLine();
+
+            Console.CursorVisible = true;
+        }
+
+        /// <summary>
+        /// display the Exit prompt on a clean screen
+        /// </summary>
+        public void DisplayExitPrompt()
+        {
+            ConsoleUtil.DisplayReset();
+
+            Console.CursorVisible = false;
+
+            Console.WriteLine();
+            ConsoleUtil.DisplayMessage("Thank you for using the application. Press any key to Exit.");
+
+            Console.ReadKey();
+
+            System.Environment.Exit(1);
+        }
+
+
+        /// <summary>
+        /// display the welcome screen
+        /// </summary>
+        public void DisplayWelcomeScreen()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("- add welcome message -");
+            Console.WriteLine();
+
+            sb.Clear();
+            sb.AppendFormat("Your first task will be to set up your account details.");
+            ConsoleUtil.DisplayMessage(sb.ToString());
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// setup the new salesperson object with the initial data
+        /// Note: To maintain the pattern of only the Controller changing the data this method should
+        ///       return a Salesperson object with the initial data to the controller. For simplicity in 
+        ///       this demo, the ConsoleView object is allowed to access the Salesperson object's properties.
+        /// </summary>
+        public void DisplaySetupAccount()
+        {
+            ConsoleUtil.HeaderText = "Account Setup";
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Setup your account now.");
+            Console.WriteLine();
+
+            ConsoleUtil.DisplayPromptMessage("Enter your first name: ");
+            _salesperson.FirstName = Console.ReadLine();
+            Console.WriteLine();
+
+            //
+            // TODO prompt the user to input all of the required account information
+            //
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display a closing screen when the user quits the application
+        /// </summary>
+        public void DisplayClosingScreen()
+        {
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("- add closing message -");
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// get the menu choice from the user
+        /// </summary>
+        public MenuOption DisplayGetUserMenuChoice()
+        {
+            MenuOption userMenuChoice = MenuOption.None;
+            bool usingMenu = true;
+
+            //
+            // TODO enable each application function separately and test
+            //
+            while (usingMenu)
+            {
+                //
+                // set up display area
+                //
+                ConsoleUtil.DisplayReset();
+                Console.CursorVisible = false;
+
+                //
+                // display the menu
+                //
+                ConsoleUtil.DisplayMessage("Please type the number of your menu choice.");
+                Console.WriteLine();
+                Console.WriteLine(
+                    "\t" + "6. Display Account Info" + Environment.NewLine +
+                    "\t" + "E. Exit" + Environment.NewLine);
+
+                //
+                // get and process the user's response
+                // note: ReadKey argument set to "true" disables the echoing of the key press
+                //
+                ConsoleKeyInfo userResponse = Console.ReadKey(true);
+                switch (userResponse.KeyChar)
+                {
+                    case '6':
+                        userMenuChoice = MenuOption.DisplayAccountInfo;
+                        usingMenu = false;
+                        break;
+                    case 'E':
+                    case 'e':
+                        userMenuChoice = MenuOption.Exit;
+                        usingMenu = false;
+                        break;
+                    default:
+                        //
+                        // TODO handle invalid menu responses from user
+                        //
+                        break;
+                }
+            }
+            Console.CursorVisible = true;
+
+            return userMenuChoice;
+        }
+        /// <summary>
+        /// get the next city to travel to from the user
+        /// </summary>
+        /// <returns>string City</returns>
+        public string DisplayGetNextCity()
+        {
+            string nextCity = "";
+
+            ConsoleUtil.HeaderText = "Next City of Travel";
+            ConsoleUtil.DisplayReset();
+
+            return nextCity;
+        }
+
+        /// <summary>
+        /// get the number of widget units to buy from the user
+        /// </summary>
+        /// <returns>int number of units to buy</returns>
+        public int DisplayGetNumberOfUnitsToBuy()
+        {
+            int numberOfUnitsToAdd = 0;
+
+            ConsoleUtil.HeaderText = "Buy Inventory";
+            ConsoleUtil.DisplayReset();
+
+            return numberOfUnitsToAdd;
+        }
+
+        /// <summary>
+        /// get the number of widget units to sell from the user
+        /// </summary>
+        /// <returns>int number of units to buy</returns>
+        public int DisplayGetNumberOfUnitsToSell()
+        {
+            int numberOfUnitsToSell = 0;
+
+            ConsoleUtil.HeaderText = "Sell Inventory";
+            ConsoleUtil.DisplayReset();
+
+            DisplayContinuePrompt();
+
+            return numberOfUnitsToSell;
+        }
+
+        /// <summary>
+        /// display the current inventory
+        /// </summary>
+        public void DisplayInventory()
+        {
+            ConsoleUtil.HeaderText = "Current Inventory";
+            ConsoleUtil.DisplayReset();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display a list of the cities traveled
+        /// </summary>
+        public void DisplayCitiesTraveled()
+        {
+            ConsoleUtil.HeaderText = "Cities Traveled To";
+            ConsoleUtil.DisplayReset();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display the current account information
+        /// </summary>
+        public void DisplayAccountInfo()
+        {
+            ConsoleUtil.HeaderText = "Account Info";
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("First Name: " + _salesperson.FirstName);
+
+            DisplayContinuePrompt();
+        }
+
+        #endregion
+    }
+}
